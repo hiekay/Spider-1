a891b590b8c3dc10fab8321a74974b4c88f04007
Lin <weihaolin2008@qq.com>
2016-08-15 22:40:35 +0800

    当当网搜索

---
+++ src/com/spider/test/ContentTest.java
@@ -0,0 +1,29 @@
+package com.spider.test;
+
+import java.util.List;
+
+import org.jsoup.Connection.Response;
+import org.jsoup.select.Elements;
+
+import com.spider.bean.LinkTypeData;
+import com.spider.bean.Rule;
+import com.spider.service.ExtractService;
+import com.spider.service.PrintService;
+
+public class ContentTest {
+	
+	public static void main(String[] args) {
+	    /*当当网商品爬取*/
+        Rule rule = new Rule("http://search.dangdang.com",  
+                new String[] {"key"}, new String[] {"白说"},  
+                "pic", Rule.CLASS, Rule.GET);
+        
+        Response response = null;
+        /*处理返回数据*/
+        Elements results = ExtractService.extract(rule, response);
+        /*获取对应的内容*/
+        List<LinkTypeData> extracts = ExtractService.searchHref(results);
+        PrintService.printf(extracts);
+	}
+
+}
