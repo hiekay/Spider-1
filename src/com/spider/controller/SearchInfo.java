6b9a45582a67615a42b7e7ce991a2b3997410ed8
Lin <weihaolin2008@qq.com>
2016-08-18 01:06:38 +0800

    查询信息的逻辑

---
+++ src/com/spider/controller/SearchInfo.java
@@ -0,0 +1,62 @@
+package com.spider.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.select.Elements;
+
+import com.spider.bean.Book;
+import com.spider.bean.Rule;
+import com.spider.service.ExtractService;
+
+public class SearchInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SearchInfo() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		
+		String url = "http://search.dangdang.com";
+		
+		/*在建立一次http握手后,保存cookie,以免每次都需建立连接*/
+		Connection con = Jsoup.connect(url).userAgent("Mozilla/5.0 "
+				+ "(Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko)"
+				+ " Chrome/26.0.1410.64 Safari/537.31");
+		Response r = con.ignoreContentType(true).method(Method.GET).execute();
+		
+	    /*当当网商品爬取*/
+        Rule rule = new Rule(url,  
+                new String[] {"key"}, new String[] {name},  
+                "pic", Rule.CLASS, Rule.GET);
+        
+        /*处理返回数据*/
+        Elements results = ExtractService.extract(rule, r);
+        /*获取对应的内容*/
+
+        List<Book> booklist = ExtractService.searchListInfo(results, r);
+
+        for (int i = 0; i < booklist.size(); i++) {
+			System.out.println(booklist.get(i).toString());
+		}
+        
+        
+	}
+
+}
